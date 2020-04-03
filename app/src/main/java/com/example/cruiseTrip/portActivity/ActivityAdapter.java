9dd33c931f02b8a4f134495931d12702e4a4dfcf
weihan <linw14@student.douglascollege.ca>
2020-04-02 17:53:04 -0700

    Fix variable scope

--- app/src/main/java/com/example/cruiseTrip/portActivity/ActivityAdapter.java
+++ app/src/main/java/com/example/cruiseTrip/portActivity/ActivityAdapter.java
@@ -28,6 +28,7 @@ public class ActivityAdapter extends ArrayAdapter {
     private final List<SpecialActivity> actityItems;
     private final List<Reservation> reservations;
     private ItineraryViewModel itineraryViewModel;
+    private int userId = 0;
 
     public ActivityAdapter(Activity context, List<SpecialActivity> item, List<Reservation> reservations){
         super(context, R.layout.activities_list,item);
@@ -62,9 +63,9 @@ public class ActivityAdapter extends ArrayAdapter {
         String username = session.getUsername();
         UsersRepository usersRepository = new UsersRepository(context.getApplication());
 
-        int userId = 0;
+
         if(usersRepository.getUser(username) != null)
-            usersRepository.getUser(username).getId();
+            userId = usersRepository.getUser(username).getId();
 
         int count = 0;
 

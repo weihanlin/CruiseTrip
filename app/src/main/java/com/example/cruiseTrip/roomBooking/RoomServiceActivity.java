af1db09c48af06f3c327f49ea3c5c4d8cb116445
dr386 <dr386.student@gmail.com>
2020-04-02 21:41:30 -0700

    little modifies

--- app/src/main/java/com/example/cruiseTrip/roomBooking/RoomServiceActivity.java
+++ app/src/main/java/com/example/cruiseTrip/roomBooking/RoomServiceActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.cruiseTrip.R;
 import com.example.cruiseTrip.adapters.ServicesAdapter;
 import com.example.cruiseTrip.database.ServiceRepository;
+import com.example.cruiseTrip.database.entity.RoomService;
 import com.example.cruiseTrip.database.entity.Service;
+import com.example.cruiseTrip.ui.InvoiceActivity;
 
 import java.util.List;
 
@@ -17,6 +21,7 @@ public class RoomServiceActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     List<Service> services;
+    private Button btn;
 
     int images[] = {R.drawable.img_roomservice_morningcall, R.drawable.img_roomservice_fooddelivery,
             R.drawable.img_roomservice_sensespa, R.drawable.img_roomservice_chillspa};
@@ -24,7 +29,7 @@ public class RoomServiceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_room_onboard);
+        setContentView(R.layout.activity_room_service);
 
         ServiceRepository repository = new ServiceRepository(getApplication());
         services = repository.getAllServices();
@@ -33,5 +38,11 @@ public class RoomServiceActivity extends AppCompatActivity {
         ServicesAdapter adapter = new ServicesAdapter(this, images, services);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        btn = findViewById(R.id.service_no);
+        btn.setOnClickListener(v -> {
+            Intent intent = new Intent(RoomServiceActivity.this, InvoiceActivity.class);
+            startActivity(intent);
+        });
     }
 }
